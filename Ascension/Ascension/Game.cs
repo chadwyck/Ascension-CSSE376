62c57160cb8846ea4f74ffb664b5ce6b2b117612
Chadwick Jones <jonescd@rose-hulman.edu>
2014-03-20 12:15:17 -0400

    Added basic VStudio project and test

---
+++ Ascension/Ascension/Game.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace Ascension
+{
+    public class Game
+    {
+        private int gameInt;
+
+        public int GameInt
+        {
+            get; private set;
+        }
+
+        public Game (int gameIntInput)
+		{
+            if (gameIntInput < 0)
+                throw new ArgumentOutOfRangeException("Game int must not be negative!");
+
+			gameInt = gameIntInput;
+		}
+    }
+}
---
+++ Ascension/UnitTestProject1/GameTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ascension;
+using NUnit.Framework;
+
+namespace AscensionTest
+{
+    [TestFixture()]
+    public class GameTest
+    {
+        private readonly int TestGameInt = 5;
+        [Test()]
+        public void TestThatGameInitializes()
+        {
+            var target = new Game(TestGameInt);
+            Assert.IsNotNull(target);
+        }
+    }
+}
